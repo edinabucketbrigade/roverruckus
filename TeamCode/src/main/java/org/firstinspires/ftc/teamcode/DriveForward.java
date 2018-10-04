3f29a787fd3710182c33bc9c9a4c822ea81f131f
Jim Musil <jimtronic@gmail.com>
2018-10-04 18:52:41 -0500

    Basic Drive Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveForward.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Drive_Forward")
+public class DriveForward extends LinearOpMode {
+
+
+    HardwareBucketBrigade hw = new HardwareBucketBrigade();
+    ElapsedTime timer = new ElapsedTime();
+
+
+    final static int RED = -1;
+    final static int BLUE = 1;
+    final static int FORWARD = 1;
+    final static int BACKWARD = -1;
+    int teamColor = RED;
+
+    double jewelSpeed = 0.5;
+    final int TIME_TO_KNOCK_OFF = 3000; // ms
+
+
+    public void driveDirection(int forwardOrBackward) {
+        hw.leftDrive.setPower(jewelSpeed * forwardOrBackward);
+        hw.rightDrive.setPower(jewelSpeed * forwardOrBackward);
+    }
+
+    int colorSeen;
+    @Override
+    public void runOpMode() {
+
+        int driveDirection = teamColor * colorSeen;
+        driveDirection(driveDirection);
+
+
+
+
+    }
+
+
+}
